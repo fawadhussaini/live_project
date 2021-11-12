2176497853cffffcae731256a534d6c66353d838
Sopisa Gorospe <sopisa.pk@gmail.com>
2021-11-11 21:42:32 -0500

    EMB-1 test done

--- src/main/java/impl/UserImpl.java
+++ src/main/java/impl/UserImpl.java
@@ -7,6 +7,19 @@ import utils.WebDriverUtils;
 
 public class UserImpl {
 
+    UserPage page;
+    LoginPage loginPage;
 
+    public UserPage getPage() {
+        if (page == null)
+            page = new UserPage();
+        return page;
+    }
+
+    public LoginPage getLoginPage(){
+        if(loginPage == null)
+            loginPage = new LoginPage();
+        return loginPage;
+    }
 
 }
--- src/main/java/pages/UserPage.java
+++ src/main/java/pages/UserPage.java
@@ -1,6 +1,8 @@
 package pages;
 
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utils.WebDriverUtils;
 
@@ -8,7 +10,23 @@ public class UserPage {
     public UserPage(){
 
         PageFactory.initElements(WebDriverUtils.getDriver(), this);
+
     }
+
+    @FindBy(xpath = "//thead/tr/th[1]")
+    public WebElement idTable;
+
+    @FindBy(xpath = "//thead/tr/th[2]")
+    public WebElement firstnameTable;
+
+    @FindBy(xpath = "//thead/tr/th[3]")
+    public WebElement lastnameTable;
+
+    @FindBy(xpath = "//thead/tr/th[4]")
+    public WebElement roleTable;
+
+    @FindBy(xpath = "//thead/tr/th[5]")
+    public WebElement departmentTable;
 
 
 }
